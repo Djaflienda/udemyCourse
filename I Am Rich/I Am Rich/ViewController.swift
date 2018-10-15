363f12cb1708fbb7c91f9d2864b643e704210b0c
Djaflienda <djaflienda@gmail.com>
2018-10-15 08:53:45 +0300

    initial iOS dev commit

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Djaflienda on 15/10/2018.
+//  Copyright © 2018 Igor Tumanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
